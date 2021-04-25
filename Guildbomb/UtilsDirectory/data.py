5c3f900ec057e6b5016bef8b304f93f38ec143df
ErnestoAndreGC <83153249+ErnestoAndreGC@users.noreply.github.com>
2021-04-24 23:22:35 -0600

    Update data.py

--- Guildbomb/UtilsDirectory/data.py
+++ Guildbomb/UtilsDirectory/data.py
@@ -5,7 +5,7 @@ import os
 
 ########################################################################################################################
 # Paste your token and a custom prefix right here:
-dc_token = "ODM1NzQwMDA3ODI1MTQ1ODY3.YIT1iQ.sOG0Z3WWPRPl_6ibWZ7QXIFB9-0"  # DISCORD-TOKEN
+dc_token = "X"  # DISCORD-TOKEN
 dc_prefix = "!!"  # BOT-PREFIX
 
 ########################################################################################################################
